e189368dcedcbdd3bf562e828e3f5ee2c19c6b02
alan <azc@live.unc.edu>
2021-01-07 13:39:54 -0500

    update discussion

--- Forum/commentcode.js
+++ Forum/commentcode.js
@@ -38,6 +38,83 @@ var defaultThreads = [
       },
     ],
   },
+  {
+    id: 3,
+    title: "I am currently at the hospital. Covid is no joke!",
+    author: "",
+    date: Date.now(),
+    content: "Thread content 2",
+    comments: [
+      {
+        author: "",
+        date: Date.now(),
+        content: "Praying for you!",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title:
+      "I am currently a nurse at Duke Hospital. ICU's are reaching capacity. Please stay home!",
+    author: "",
+    date: Date.now(),
+    content: "Thread content 2",
+    comments: [],
+  },
+  {
+    id: 5,
+    title:
+      "I am a college student who got covid. I couldn't taste anything for a month.",
+    author: "",
+    date: Date.now(),
+    content: "Thread content 2",
+    comments: [
+      {
+        author: "",
+        date: Date.now(),
+        content: "I hope you get well soon.",
+      },
+      {
+        author: "",
+        date: Date.now(),
+        content: "That sucks man.",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "I am experiencing a running nose. Should I get tested?",
+    author: "",
+    date: Date.now(),
+    content: "Thread content 2",
+    comments: [
+      {
+        author: "",
+        date: Date.now(),
+        content: "wait a couple days to see if it gets worse.",
+      },
+    ],
+  },
+  {
+    id: 7,
+    title:
+      "If you are a university student, they should have free testing there!",
+    author: "",
+    date: Date.now(),
+    content: "Thread content 2",
+    comments: [
+    ],
+  },
+  {
+    id: 8,
+    title:
+      "I am a doctor. Please don't do anything stupid over the holidays.",
+    author: "",
+    date: Date.now(),
+    content: "Thread content 2",
+    comments: [
+    ],
+  },
 ];
 
 var threads = defaultThreads;
